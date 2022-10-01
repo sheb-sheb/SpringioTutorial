569214a1738f38e620ddae5498cdb4bc875d8961
sheba <sheba8367@gmail.com>
2022-10-01 11:54:51 -0700

    Add Unit Tests

---
+++ src/main/java/com/example/springboot/Application.java
@@ -0,0 +1,38 @@
+package com.example.springboot;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+/*
+@SpringBootApplication adds all of following
+- Configuration - makes class source of bean definitions
+- @EnableAutoConfiguration - adds beans based on classpath, settings, beans and property settings
+- @ComponentScan - look for components, configs, and services, letting it find controllers
+ */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);//launches application without xml
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+
+}
